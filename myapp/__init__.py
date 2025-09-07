fd49493bec6c3875393362d07aef005206a37d5e
padole <inas4wealth@gmail.com>
2025-09-07 18:32:37 +0100

    Email notification update

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -13,20 +13,7 @@ def create_app():
     from myapp.models import db, User, Admin # Import the db instance from SQLALCHEMY
     template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates'))
     app = Flask(__name__, template_folder=template_dir, instance_relative_config=True) # Load config from instance folder
-    # app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # or your SMTP server
-    # app.config['MAIL_PORT'] = 587
-    # app.config['MAIL_USE_TLS'] = True
-    # app.config['MAIL_USERNAME'] = 'inas4wealth@gmail.com'
-    # app.config['MAIL_PASSWORD'] = 'dvtm yghs twqu rgpj'  # or your email password
-    # app.config['MAIL_DEFAULT_SENDER'] = ('Ladol Access', 'inas4wealth@gmail.com')
-
-
-    app.config['MAIL_SERVER'] = 'smtp.office365.com'  # or your SMTP server
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = 'meetings@ladol.com'
-    app.config['MAIL_PASSWORD'] = 'Kah37418'  # or your email password
-    app.config['MAIL_DEFAULT_SENDER'] = ('Ladol Access', 'meetings@ladol.com')
+    
     app.config.from_pyfile('config.py')
     
     # app.config.from_object(config.TestConfig)# Load config from class
